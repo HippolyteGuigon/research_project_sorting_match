7d6026df9a8e4e8ae0bbad7fcbdd2f7b6aa24b27
HippolyteGuigon <guigonhippolyte@yahoo.fr>
2025-07-27 20:56:49 +0200

    added passes count

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,10 +2,10 @@ from typing import List
 
 def bubble_sort(array:List[int])->List[int]:
     n=len(array)
-
+    count_pass=0
     for i in range(n-1):
         for j in range(0,n-i-1):
             if array[j]>array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
-    
-    return array
+        count_pass+=1
+    return array,count_pass
